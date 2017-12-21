fe74cfd58cc1b80a0c846f7713052ae201ac933d
Joe Wemyss <support@javathenutt.com>
2017-12-21 16:17:05 +0000

    configured testcafe to use runner

--- test/unit/specs/Hello.spec.js
+++ test/unit/specs/Hello.spec.js
@@ -1,11 +1,9 @@
-import Vue from 'vue';
 import Hello from '@/components/Hello';
+import { shallow } from 'vue-test-utils';
 
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello);
-    const vm = new Constructor().$mount();
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js PWA');
+    const wrapper = shallow(Hello);
+    expect(wrapper.text()).to.contain('First, solve the problem. Then, write the code');
   });
 });
