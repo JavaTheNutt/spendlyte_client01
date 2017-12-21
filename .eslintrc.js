5a797550d899062649e90052c6b9b2c994cc31dd
Joe Wemyss <support@javathenutt.com>
2017-12-21 10:16:40 +0000

    created eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,40 @@ module.exports = {
   // add your custom rules here
   'rules': {
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "no-useless-concat": "error",
+    "require-jsdoc": "error",
+    "require-await": "error",
+    "arrow-spacing":"error",
+    "prefer-rest-params":"error",
+    "arrow-parens": [
+      "error",
+      "as-needed"
+    ],
+    "arrow-body-style": [
+      "error",
+      "as-needed"
+    ],
+    "indent": [
+      "error",
+      2
+    ],
+    "no-var": "error",
+    "prefer-const": "error",
+    "prefer-arrow-callback": "error",
+    "consistent-return": "error",
+    "eqeqeq": "error",
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "object-shorthand": [
+      "error",
+      "always"
+    ]
   }
-}
+};
