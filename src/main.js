0374b8110baf1c3956d4845c1e13453b61d8a93a
Joe Wemyss <support@javathenutt.com>
2017-12-21 18:08:23 +0000

    removed default components

--- src/main.js
+++ src/main.js
@@ -5,7 +5,19 @@ import 'vuetify/dist/vuetify.css';
 import App from './app/App';
 import router from './router';
 
-Vue.use(Vuetify);
+import colors from 'vuetify/es5/util/colors';
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: colors.blue.base,
+    accent: colors.red.accent2,
+    secondary: colors.grey.lighten1,
+    info: colors.blue.lighten1,
+    warning: colors.amber.darken2,
+    error: colors.red.accent4,
+    success: colors.green.lighten2
+  }
+});
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
