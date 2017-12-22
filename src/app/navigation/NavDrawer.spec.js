da3acbb9a98c3c670e4004974bef6bb28aafe611
Joe Wemyss <support@javathenutt.com>
2017-12-22 14:11:03 +0000

    tested redirect function

--- src/app/navigation/NavDrawer.spec.js
+++ src/app/navigation/NavDrawer.spec.js
@@ -1,23 +1,27 @@
 import NavDrawer from './NavDrawer';
-import { shallow } from 'vue-test-utils';
+import { shallow, createLocalVue } from 'vue-test-utils';
 import NavBus from './navBus';
+
 const sandbox = sinon.sandbox.create();
 describe('NavDrawer.vue', () => {
-  let onStub;
+  let onStub, pushStub, mockedRouter, localVue;
+  const items = [{
+    title: 'home',
+    route: '/',
+    icon: 'home'
+  }, {
+    title: 'profile',
+    route: '/profile',
+    icon: 'user'
+  }];
   beforeEach(() => {
     onStub = sandbox.stub(NavBus, '$on');
+    pushStub = sandbox.stub();
+    mockedRouter = { push: pushStub };
+    localVue = createLocalVue();
   });
   afterEach(() => sandbox.restore());
   it('should render a tile for each route passed to it', () => {
-    const items = [{
-      title: 'home',
-      route: '/',
-      icon: 'home'
-    }, {
-      title: 'profile',
-      route: '/profile',
-      icon: 'user'
-    }];
     const wrapper = shallow(NavDrawer, { propsData: { items }});
     const tiles = wrapper.findAll('v-list-tile');
     expect(tiles).to.have.length.of(2);
@@ -26,5 +30,19 @@ describe('NavDrawer.vue', () => {
     shallow(NavDrawer);
     expect(onStub).to.be.calledOnce;
     expect(onStub).to.be.calledWith('toggle_drawer_button_clicked');
+  });
+  it('should call redirect when a link is clicked', () => {
+    const wrapper = shallow(NavDrawer, {
+      localVue,
+      beforeCreate () {
+        this._router = mockedRouter;
+      },
+      propsData: {
+        items
+      }
+    });
+    const link = wrapper.find('#link-home');
+    link.trigger('click');
+    expect(pushStub).to.be.calledWith('/');
   });
 });
