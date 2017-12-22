4fe3e8074f85ec89c499b087721ca0cb54888c96
Joe Wemyss <support@javathenutt.com>
2017-12-22 11:01:37 +0000

    catch event in drawer

--- src/app/navigation/NavDrawer.spec.js
+++ src/app/navigation/NavDrawer.spec.js
@@ -1,6 +1,14 @@
 import NavDrawer from './NavDrawer';
 import { shallow } from 'vue-test-utils';
+import NavBus from './navBus';
+import Vue from 'vue';
+const sandbox = sinon.sandbox.create();
 describe('NavDrawer.vue', () => {
+  let busMock;
+  beforeEach(() => {
+    busMock = sandbox.mock(NavBus);
+  });
+  afterEach(() => sandbox.restore());
   it('should render a tile for each route passed to it', () => {
     const items = [{
       title: 'home',
@@ -14,5 +22,14 @@ describe('NavDrawer.vue', () => {
     const wrapper = shallow(NavDrawer, { propsData: { items }});
     const tiles = wrapper.findAll('v-list-tile');
     expect(tiles).to.have.length.of(2);
+  });
+  it('should register an event handler on creation', () => {
+    expect(typeof NavDrawer.created).to.equal('function');
+    busMock.expects('$on').once();
+    busMock.expects('$on').withArgs('toggle_drawer_button_clicked');
+    shallow(NavDrawer);
+    Vue.nextTick(() => {
+      busMock.verify();
+    });
   });
 });
