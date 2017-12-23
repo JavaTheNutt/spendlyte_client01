374f4524db5c29d6846e043266197fd8d55494fb
Joe Wemyss <support@javathenutt.com>
2017-12-23 22:46:56 +0000

    created get authorized routes function

--- src/app/navigation/service/navigationService.js
+++ src/app/navigation/service/navigationService.js
@@ -1,19 +1,11 @@
 import { routeMeta as links } from '@/app';
+import store from '@/store';
 
-/* const NAV_LINKS = [{
-  title: 'Home',
-  route: '/',
-  icon: 'home',
-  shown: true
-}, {
-  title: 'Profile',
-  route: '/profile',
-  icon: 'supervisor_account',
-  shown: true
-}];*/
 export const getAllNavLinks = () => links;
 
 export const fetchShownLinks = links => links.filter(link => link.shown);
 
 export const fetchSideNavLinks = () => fetchShownLinks(getAllNavLinks());
+
+export const getAuthorizedRoutes = () => links.filter(link => link.shown ? true : store.state.auth.loggedIn);
 
--- src/app/navigation/service/navigationService.spec.js
+++ src/app/navigation/service/navigationService.spec.js
@@ -1,55 +1,85 @@
-let navService;
-
 const routesInjector = require('inject-loader!./navigationService');
 const sandbox = sinon.sandbox.create();
-describe('nav service', () => {
-  beforeEach(() => {
-    navService = routesInjector({
-      '@/app': {
-        routeMeta: [{
-          title: 'Home',
-          icon: 'home',
-          path: '/home',
-          shown: true
-        }, {
-          title: 'Fake',
-          icon: 'supervisor_account',
-          path: '/fake',
-          shown: false
-        }]
+const fakeRoutes = {
+  routeMeta: [{
+    title: 'Home',
+    icon: 'home',
+    path: '/home',
+    shown: true
+  }, {
+    title: 'Fake',
+    icon: 'supervisor_account',
+    path: '/fake',
+    shown: false
+  }]
+};
+const navServiceLoggedIn = routesInjector({
+  '@/app': fakeRoutes,
+  '@/store': {
+    state: {
+      auth: {
+        loggedIn: true
       }
-    });
-  });
+    }
+  }
+});
+const navServiceLoggedOut = routesInjector({
+  '@/app': fakeRoutes,
+  '@/store': {
+    state: {
+      auth: {
+        loggedIn: false
+      }
+    }
+  }
+});
+describe('nav service', () => {
   afterEach(() => {
     sandbox.restore();
   });
-  describe('get all nav links', () => {
-    it('should return an array of navigation links', () => {
-      const result = navService.getAllNavLinks();
-      console.log(JSON.stringify(result));
-      result.forEach(nav => {
-        expect(Object.keys(nav)).to.include.members(['title', 'path', 'icon', 'shown']);
-        expect(Object.keys(nav).length).to.equal(4);
+  describe('no auth', () => {
+    describe('get all nav links', () => {
+      it('should return an array of navigation links', () => {
+        const result = navServiceLoggedIn.getAllNavLinks();
+        console.log(JSON.stringify(result));
+        result.forEach(nav => {
+          expect(Object.keys(nav)).to.include.members(['title', 'path', 'icon', 'shown']);
+          expect(Object.keys(nav).length).to.equal(4);
+        });
       });
     });
-  });
-  describe('get shown links', () => {
-    it('should return all of the shown links', () => {
-      const links = Object.assign([], navService.getAllNavLinks());
-      const result = navService.fetchShownLinks(links);
-      result.forEach(link => {
-        expect(link).to.not.eql({
-          title: 'Fake',
-          icon: 'supervisor_account',
-          path: '/fake',
-          shown: 'false'
+    describe('get shown links', () => {
+      it('should return all of the shown links', () => {
+        const links = Object.assign([], navServiceLoggedIn.getAllNavLinks());
+        const result = navServiceLoggedIn.fetchShownLinks(links);
+        result.forEach(link => {
+          expect(link).to.not.eql({
+            title: 'Fake',
+            icon: 'supervisor_account',
+            path: '/fake',
+            shown: 'false'
+          });
         });
       });
     });
   });
   describe('filter links', () => {
     describe('user logged in', () => {
-
+      it('should return all links', () => {
+        const result = navServiceLoggedIn.getAuthorizedRoutes();
+        expect(result).to.eql(fakeRoutes.routeMeta);
+      });
+    });
+    describe('user logged out', () => {
+      it('should only return the links that have shown set to true', () => {
+        const result = navServiceLoggedOut.getAuthorizedRoutes();
+        expect(result).to.eql([{
+          title: 'Home',
+          icon: 'home',
+          path: '/home',
+          shown: true
+        }]);
+      });
     });
   });
 });
