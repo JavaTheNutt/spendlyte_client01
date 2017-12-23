c5f82f0b64774a6f887f4cd07148ee89de89280f
Joe Wemyss <support@javathenutt.com>
2017-12-23 22:04:15 +0000

    stubbed route data for unit tests

--- src/app/navigation/service/navigationService.spec.js
+++ src/app/navigation/service/navigationService.spec.js
@@ -1,13 +1,32 @@
-import * as navService from './navigationService';
+let navService;
 
+const routesInjector = require('inject-loader!./navigationService');
 const sandbox = sinon.sandbox.create();
 describe('nav service', () => {
+  beforeEach(() => {
+    navService = routesInjector({
+      '@/app': {
+        routeMeta: [{
+          title: 'Home',
+          icon: 'home',
+          path: '/home',
+          shown: true
+        }, {
+          title: 'Fake',
+          icon: 'supervisor_account',
+          path: '/fake',
+          shown: false
+        }]
+      }
+    });
+  });
   afterEach(() => {
     sandbox.restore();
   });
   describe('get all nav links', () => {
     it('should return an array of navigation links', () => {
       const result = navService.getAllNavLinks();
+      console.log(JSON.stringify(result));
       result.forEach(nav => {
         expect(Object.keys(nav)).to.include.members(['title', 'path', 'icon', 'shown']);
         expect(Object.keys(nav).length).to.equal(4);
@@ -17,40 +36,20 @@ describe('nav service', () => {
   describe('get shown links', () => {
     it('should return all of the shown links', () => {
       const links = Object.assign([], navService.getAllNavLinks());
-      const fakeLink = {
-        title: 'fake',
-        route: '/fake',
-        icon: 'fake',
-        shown: false
-      };
-      links.push(fakeLink);
       const result = navService.fetchShownLinks(links);
       result.forEach(link => {
-        expect(link).to.not.eql(fakeLink);
+        expect(link).to.not.eql({
+          title: 'Fake',
+          icon: 'supervisor_account',
+          path: '/fake',
+          shown: 'false'
+        });
       });
     });
   });
-  /* describe('fetch side nav links', () => {
-    it('should return the nav links that have shown = true', () => {
-      const fetchLinksStub = sandbox.stub(navService, 'getAllNavLinks');
-      fetchLinksStub.returns([{
-        title: 'home',
-        route: '/',
-        icon: 'home',
-        shown: true
-      }, {
-        title: 'fake',
-        route: '/fake',
-        icon: 'fake',
-        shown: false
-      }]);
-      const result = navService.fetchSideNavLinks();
-      expect(result).to.eql([{
-        title: 'home',
-        route: '/',
-        icon: 'home',
-        shown: true
-      }]);
+  describe('filter links', () => {
+    describe('user logged in', () => {
+
     });
-  });*/
+  });
 });
