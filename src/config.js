6b617f5a60fbaa63d6c4fc4716ec5b2c8eb052d1
Joe Wemyss <support@javathenutt.com>
2018-01-05 23:44:08 +0000

    sync router with store

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 import router from './router';
 import store from './store';
+import { sync } from 'vuex-router-sync';
 import { globalAuthGuard } from './router/guards';
 
+sync(store, router, { moduleName: 'router_store' });
 router.beforeEach(globalAuthGuard);
 export default {
   store,
