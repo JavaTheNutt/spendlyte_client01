bcc504a11f2b14a38f0c6632b85c5ba91ae114c0
Joe Wemyss <support@javathenutt.com>
2018-01-06 00:46:08 +0000

    created sign out function

--- src/app/auth/service/FirebaseAuthService.js
+++ src/app/auth/service/FirebaseAuthService.js
@@ -1,7 +1,8 @@
+// @flow
 import * as Logger from 'loglevel';
 import firebase from 'firebase';
 
-export const signUpWithEmailAndPassword = async (email, password) => {
+export const signUpWithEmailAndPassword = async (email: String, password: String) => {
   Logger.info('attempting to sign in with email and password');
   try {
     await firebase.auth().createUserWithEmailAndPassword(email, password);
@@ -16,4 +17,16 @@ export const signUpWithEmailAndPassword = async (email, password) => {
 export const registerAuthStateListener = () => {
   Logger.info('registering auth state listener');
   firebase.auth().onAuthStateChanged(user => Logger.info('user logged in: ', user));
+};
+
+export const signOut = async () => {
+  Logger.info('sign out function triggered');
+  try {
+    await firebase.auth().signOut();
+    Logger.debug('sign out assumed successful');
+    return true;
+  } catch (e) {
+    Logger.warn('error while signing out', e);
+    return false;
+  }
 };
