9bd4791c44a157be9f6316f50cad1fdfc2ff7883
Joe Wemyss <support@javathenutt.com>
2018-01-09 14:22:32 +0000

    updated timestamp formatting

--- test/e2e/specs/auth.js
+++ test/e2e/specs/auth.js
@@ -2,7 +2,8 @@ import { expect } from 'chai';
 import { Selector } from 'testcafe';
 import VueSelector from 'testcafe-vue-selectors';
 import * as firebaseUtil from '../util/firebaseAuth';
-
+const currentDate = new Date();
+const currentTimestamp = `${currentDate.getMonth() + 1}-${currentDate.getDate()}-${currentDate.getTime()}`;
 fixture `test authentication`.page(`http://localhost:${process.env.PORT}`);
 
 test('log in', async t => {
@@ -10,21 +11,21 @@ test('log in', async t => {
   const emailField = Selector('#emailField');
   expect(await emailField.exists).to.be.true;
   await t.typeText('#emailField', 'i')
-  .takeScreenshot(`invalid-email-${new Date()}.png`)
+  .takeScreenshot(`invalid-email-${currentTimestamp}.png`)
   .typeText('#emailField', 'amtheuserthatisusedforacceptancetests@test.com')
-  .takeScreenshot(`valid-email-${new Date()}.png`);
+  .takeScreenshot(`valid-email-${currentTimestamp}.png`);
   const passwordField = Selector('#passwordField');
   expect(await passwordField.exists).to.be.true;
   await t.typeText('#passwordField', 'w')
-  .takeScreenshot(`invalid-password-${new Date()}.png`)
+  .takeScreenshot(`invalid-password-${currentTimestamp}.png`)
   .typeText('#passwordField', 'wwwww')
-  .takeScreenshot(`valid-password-${new Date()}.png`);
-  await t.takeScreenshot(`valid-login-${new Date()}.png`);
+  .takeScreenshot(`valid-password-${currentTimestamp}.png`);
+  await t.takeScreenshot(`valid-login-${currentTimestamp}.png`);
   const submitButton = Selector('#submitButton');
   await t.click(submitButton);
-  await t.takeScreenshot(`login-form-loading-${new Date()}.png`);
+  await t.takeScreenshot(`login-form-loading-${currentTimestamp}.png`);
   const loginForm = VueSelector('email-password-form');
-  await t.wait(2000).takeScreenshot(`logged-in-${new Date()}.png`);
+  await t.wait(2000).takeScreenshot(`logged-in-${currentTimestamp}.png`);
   expect(await loginForm.exists).to.be.false;
 }).before(async t => await firebaseUtil.addUser());
 
@@ -39,13 +40,13 @@ test('sign up', async t => {
   const newAccountCheckbox = Selector('.material-icons.icon.icon--selection-control');
   expect(await newAccountCheckbox.exists).to.be.true;
   await t.click(newAccountCheckbox)
-  .takeScreenshot(`new-account-ticked-${new Date()}.png`);
+  .takeScreenshot(`new-account-ticked-${currentTimestamp}.png`);
   const confirmPasswordField = Selector('#confirmPasswordField');
   expect(await confirmPasswordField.exists).to.be.true;
   await t.typeText(confirmPasswordField, 'w')
-  .takeScreenshot(`invalid-confirm-password-${new Date()}.png`)
+  .takeScreenshot(`invalid-confirm-password-${currentTimestamp}.png`)
   .typeText(confirmPasswordField, 'wwwww')
-  .takeScreenshot(`valid-confirm-password-${new Date()}.png`);
+  .takeScreenshot(`valid-confirm-password-${currentTimestamp}.png`);
   const submitButton = Selector('#submitButton');
   await t.click(submitButton);
   const loginForm = VueSelector('email-password-form');
--- test/e2e/specs/navigation.js
+++ test/e2e/specs/navigation.js
@@ -4,6 +4,9 @@ import { Selector } from 'testcafe';
 import { logIn } from '../util/uiUtils';
 import { getLocation } from '../util/browserUtils';
 import { addUser } from '../util/firebaseAuth';
+const currentDate = new Date();
+const currentTimestamp = `${currentDate.getMonth() + 1}-${currentDate.getDate()}-${currentDate.getTime()}`;
+console.log('currentTimestamp', currentTimestamp);
 
 fixture `test navigation`.page(`http://localhost:${process.env.PORT}`).before(async t => await addUser());
 
@@ -28,10 +31,10 @@ test('toggle open close drawer', async t => {
 test('navigate', async t => {
   await logIn(t);
   const navData = await openDrawer(t);
-  await t.takeScreenshot(`nav-open-profile-${new Date()}.png`);
+  await t.takeScreenshot(`nav-open-profile-${currentTimestamp}.png`);
   const homeLink = Selector('#link-Home');
   await t.click(homeLink)
-  .takeScreenshot(`nav-open-home-${new Date()}.png`);
+  .takeScreenshot(`nav-open-home-${currentTimestamp}.png`);
   const loc = await getLocation();
   expect(loc[loc.length - 1]).equals('/');
   let navDrawerVue = await navData.getVue();
