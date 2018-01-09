9f920052ff7fc38d7582c6284f36c3a2db5df88c
Joe Wemyss <support@javathenutt.com>
2018-01-09 21:20:21 +0000

    added about page

---
+++ src/app/screens/home/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container fluid>
+    <v-layout>
+      <v-flex>
+        <p class="headline text-xs-center" id="homeHeader">Welcome to Spend Lyte!</p>
+      </v-flex>
+    </v-layout>
+    <v-layout row justify-center>
+      <v-flex xs12 md8>
+        <p class="body-1">
+          This application is designed to help you to keep track of your personal finances. If you are seeing this screen, then you are either a beta tester, or you have stumbled onto this site by accident. Either way, enjoy! If something appears to be broken, please don't hesitate to <a
+          href="mailto:support@javathenutt.com" target="_blank" rel="noopener"> contact me</a></p>
+        <p class="body-1">
+          I am building this application for my Final Year project for college. The app is a work in progress so please bear with me. You may also notice that if you visit this site several times, you will be prompted to install this application on your phone (as of now, iOS doesn't support this technology, yet). I would be very grateful if you could do this, so that I know that my application will work across multiple platforms.
+        </p>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+  export default {
+    name: 'about-view'
+  };
+</script>
--- src/app/screens/routes.js
+++ src/app/screens/routes.js
@@ -1,12 +1,23 @@
 const Home = () => import(
   /* webpackChunkName: home-view */ './home/Home'
   );
+const About = () => import(
+  /* webpackChunkName: about-view */ './home/About'
+  );
 
 export default [{
   name: 'home',
   path: '/',
   component: Home,
   icon: 'home',
+  meta: {
+    noAuth: true
+  }
+}, {
+  name: 'about',
+  path: '/about',
+  component: About,
+  icon: 'lightbulb_outline',
   meta: {
     noAuth: true
   }
