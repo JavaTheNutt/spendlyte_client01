489e965bf921ee811690db25ae9646573355deea
Joe Wemyss <support@javathenutt.com>
2018-01-09 02:11:29 +0000

    added firebase admin functionality to tests to ensure isolation

--- config/test.env.js
+++ config/test.env.js
@@ -3,5 +3,7 @@ var devEnv = require('./dev.env')
 console.log(`log level ${process.env.LOG_LEVEL}`);
 module.exports = merge(devEnv, {
   NODE_ENV: '"test"',
-  LOG_LEVEL: JSON.stringify(process.env.LOG_LEVEL) || '"silent"'
+  LOG_LEVEL: JSON.stringify(process.env.LOG_LEVEL) || '"silent"',
+  FIREBASE_PRIVATE_KEY: JSON.parse(`${process.env.FIREBASE_PRIVATE_KEY}`),
+  FIREBASE_CLIENT_EMAIL: JSON.stringify(process.env.FIREBASE_CLIENT_EMAIL)
 });
--- test/e2e/specs/auth.js
+++ test/e2e/specs/auth.js
@@ -1,7 +1,27 @@
 import { expect } from 'chai';
 import { Selector } from 'testcafe';
 import VueSelector from 'testcafe-vue-selectors';
-
+import admin from 'firebase-admin';
+const strippedKey = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n');
+/* fixme: Error: Credential implementation provided to initializeApp() via the "credential" property failed to fetch a
+   valid Google OAuth2 access token with the following error: "Error fetching access token: invalid_client (The
+   OAuth client was not found.)"
+   Need to add rest of properties from key?*/
+admin.initializeApp({
+  credential: admin.credential.cert({
+    'type': process.env.FIREBASE_TYPE,
+    'project_id': process.env.FIREBASE_PROJECT_ID,
+    'private_key_id': process.env.FIREBASE_PRIVATE_KEY_ID,
+    'private_key': strippedKey,
+    'client_email': process.env.FIREBASE_CLIENT_EMAIL,
+    'client_id': process.env.FIREBASE_CLIENT_ID,
+    'auth_uri': process.env.FIREBASE_AUTH_URI,
+    'token_uri': process.env.FIREBASE_TOKEN_URI,
+    'auth_provider_x509_cert_url': process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    'client_x509_cert_url': process.env.FIREBASE_CLIENT_X509_CERT_URL
+  }),
+  databaseURL: JSON.stringify(process.env.FIREBASE_DATABASE_URL)
+});
 fixture `test authentication`.page(`http://localhost:${process.env.PORT}`);
 
 test('log in', async t => {
@@ -17,6 +37,9 @@ test('log in', async t => {
   const loginForm = VueSelector('email-password-form');
   await t.wait(2000);
   expect(await loginForm.exists).to.be.false;
+}).before(async t => {
+  const user = await admin.auth().getUserByEmail('root@root.com');
+  if (!user) await admin.auth().createUser({ email: 'root@root.com', password: 'wwwwww' });
 });
 test('sign up', async t => {
   await openLoginDialog(t);
@@ -37,6 +60,9 @@ test('sign up', async t => {
   const loginForm = VueSelector('email-password-form');
   await t.wait(2000);
   expect(await loginForm.exists).to.be.false;
+}).before(async t => {
+  const user = await admin.auth().getUserByEmail('iamtheuserthatisusedforacceptancetests@test.com');
+  if (user) await admin.auth().deleteUser(user.uid);
 });
 async function openLoginDialog (t) {
   const loginButton = Selector('#loginButton');
