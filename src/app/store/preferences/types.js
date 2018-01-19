00b49a0c1273abf485ef6b5ebbb5bf41fbb87c93
Joe Wemyss <support@javathenutt.com>
2018-01-19 11:20:09 +0000

    updated types file in preferences store

--- src/app/store/preferences/types.js
+++ src/app/store/preferences/types.js
@@ -2,6 +2,6 @@ import namespace from '@/app/util/namespace';
 
 export default namespace('preferences', {
   getters: ['isTrustedDevice', 'doAskTrusted'],
-  actions: ['testTrustedDevice', 'trustDevice'],
-  mutations: ['SET_TRUSTED_DEVICE', 'SET_ASK_TRUSTED', 'TRUST_DEVICE']
+  actions: ['testTrustedDevice', 'trustDevice', 'untrustDevice', 'doNotAskToTrustDevice'],
+  mutations: ['SET_TRUSTED_DEVICE', 'SET_ASK_TRUSTED', 'TRUST_DEVICE', 'UNTRUST_DEVICE']
 });
