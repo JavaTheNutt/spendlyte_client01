adced644fcbef5e73277e085ada6af607fe5d4f3
Joe Wemyss <support@javathenutt.com>
2018-02-02 16:50:34 +0000

    created auth service
    created auth service module to handle common auth utilities throughout the application lifecycle
    
    currently just contains an abstraction over the fetch ID function in Firebase

---
+++ src/app/service/auth/authService.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+export const fetchIdToken = async () => {
+  console.log('request made to fetch ID token');
+  try {
+    const token = await firebase.auth().currentUser.getIdToken(true);
+    console.log('token fetched');
+    return { success: true, data: token };
+  } catch (error) {
+    console.log('there was an error fetching the token');
+    return { success: false, msg: 'there was an error fetching the token', error };
+  }
+};
