5880ad2a746a3efd113417ab707ae6002088f8ab
Joe Wemyss <support@javathenutt.com>
2018-02-20 00:51:11 +0000

    Added http fetch functions
    Created functions that request future transactions from the backend. These functions can fetch incomes, expenditures, or both from the backend to be displayed to the user.

--- src/app/data/http/finance/index.js
+++ src/app/data/http/finance/index.js
@@ -1,11 +1,37 @@
 import { http } from '@/http';
 import { fetchHeaders } from '@/http/util';
 
-export const fetchAllIncomes = async () => {
+export const fetchAllFutureIncomes = async (num = 1, skip = 0) => {
   console.log('making request to fetch all incomes');
   const headers = await fetchHeaders();
   console.log('headers', headers.data);
-  const response = await http.get('/income', { headers: headers.data });
+  const params = new URLSearchParams();
+  params.append('num', num);
+  params.append('skip', skip);
+  const response = await http.get('/income', { headers: headers.data, params });
+  console.log('response', response);
+  return response;
+};
+
+export const fetchAllFutureExpenditures = async (num = 1, skip = 0) => {
+  console.log('making request to fetch all incomes');
+  const headers = await fetchHeaders();
+  console.log('headers', headers.data);
+  const params = new URLSearchParams();
+  params.append('num', num);
+  params.append('skip', skip);
+  const response = await http.get('/expenditure', { headers: headers.data, params });
+  console.log('response', response);
+  return response;
+};
+export const fetchAllFutureTransactions = async (num = 1, skip = 0) => {
+  console.log('making request to fetch all incomes');
+  const headers = await fetchHeaders();
+  console.log('headers', headers.data);
+  const params = new URLSearchParams();
+  params.append('num', num);
+  params.append('skip', skip);
+  const response = await http.get('/all', { headers: headers.data, params });
   console.log('response', response);
   return response;
 };
