7a81c5cc729d437db72ff2b5d127ebea2da05788
Joe Wemyss <support@javathenutt.com>
2018-02-20 01:13:06 +0000

    Created Manage Finace View Component
    Created a generic wrapper component that will be used to wrap both the transaction table and management cards. This wrapper utilizes a flexile grid to make the tow child components repsonsive on mobile.

---
+++ src/app/uiComponents/finance/components/ManageFinanceView.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container fluid grid-list-md>
+    <v-layout row wrap>
+      <v-flex md6 xs12>
+        <display-finance-table
+          :items="transactions"
+          :loading="loading"
+          :transaction-type="transactionType"
+          :mixed="mixed"
+        />
+      </v-flex>
+      <v-flex md6 xs12>
+        <manage-finance-card
+          :loading="loading"
+          :transactions="transactions"
+          :mixed="mixed"
+          :shown-months="shownMonths"
+          @fetch-next="$emit('fetch-next')"
+          @add-item="$emit('add-item')"
+        />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+<script>
+  import ManageFinanceCard from './ManageFinanceCard';
+  import DisplayFinanceTable from './DisplayFinanceTable';
+  export default {
+    name: 'manage-finance-view',
+    components: {
+      ManageFinanceCard,
+      DisplayFinanceTable
+    },
+    props: {
+      transactions: Array,
+      loading: Boolean,
+      transactionType: {
+        type: String,
+        default: 'Transactions'
+      },
+      mixed: {
+        type: Boolean,
+        default: false
+      },
+      shownMonths: Number
+    }
+  };
+</script>
