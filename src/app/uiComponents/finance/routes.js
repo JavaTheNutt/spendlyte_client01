343f68fe65d201ca34dac197b7fa86065533d6fb
Joe Wemyss <support@javathenutt.com>
2018-02-20 01:18:48 +0000

    wired new views to router

--- src/app/uiComponents/finance/routes.js
+++ src/app/uiComponents/finance/routes.js
@@ -1,11 +1,13 @@
 import AddIncomeView from './components/AddIncomeView';
 import AddExpenditureView from './components/AddExpenditureView';
 import DisplayIncomeView from './components/DisplayIncomeView';
+import DisplayExpenditureView from './components/DisplayExpenditureView';
+import DisplayTransactionView from './components/DisplayTransactionView';
 
 export default [{
   name: 'income',
   path: '/income',
-  redirect: '/income/new',
+  redirect: '/income/all',
   icon: 'attach_money arrow_left'
 }, {
   name: 'add-income',
@@ -16,10 +18,19 @@ export default [{
   path: '/income/all',
   component: DisplayIncomeView
 }, {
+  name: 'display-expenditure',
+  path: '/expenditure/all',
+  component: DisplayExpenditureView
+}, {
   name: 'expenditure',
   path: '/expenditure',
-  redirect: '/expenditure/new',
+  redirect: '/expenditure/all',
   icon: 'attach_money arrow_right'
+}, {
+  name: 'transaction',
+  path: '/transactions/all',
+  component: DisplayTransactionView,
+  icon: 'attach_money'
 }, {
   name: 'add-expenditure',
   path: '/expenditure/new',
