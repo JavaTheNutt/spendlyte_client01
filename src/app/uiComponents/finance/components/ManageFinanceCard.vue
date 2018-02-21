0e4bd1e0ac51c36b34a565ec29f2aa3da4ee63be
Joe Wemyss <support@javathenutt.com>
2018-02-21 08:04:34 +0000

    Restructured ManageFinanceCard
    Restructured the manage finance card to make use of the new filter component and stats component. The markup and logic for these two child components was originally contained within this component, which led to the component being bloated. Now this component handles how these children should be displayed as a single entity

--- src/app/uiComponents/finance/components/ManageFinanceCard.vue
+++ src/app/uiComponents/finance/components/ManageFinanceCard.vue
@@ -1,50 +1,68 @@
 <template>
-  <v-card>
-    <v-container v-if="transactions.length === 0">
-      <v-progress-circular color="primary" indeterminate/>
-    </v-container>
-  <v-container v-if="transactions.length !== 0">
+  <v-container justify-center fluid>
     <v-layout column>
-      <v-flex>
-        <v-data-table
-          hide-headers
-          hide-actions
-          :items="mixed ? mixedStats : stats"
-        >
-          <template slot="items" slot-scope="props">
-            <td><strong>{{props.item.title}}</strong></td>
-            <td v-if="props.item.format === 'currency'">{{props.item.value | currency}}</td>
-            <td v-if="props.item.format === 'date'">{{props.item.value | long-date}}</td>
-            <td v-if="props.item.format === 'basic'">{{props.item.value}}</td>
-          </template>
-        </v-data-table>
+      <v-flex v-if="loading">
+        <v-progress-circular color="primary" indeterminate/>
       </v-flex>
-      <v-flex>
-        <v-card-actions>
-          <v-btn color="primary" @click.stop="fetchNext" :disabled="loading">Fetch more records</v-btn>
-          <v-btn color="primary" @click.stop="addItemClicked" :disabled="loading" v-if="!mixed">Add new item</v-btn>
-          <v-btn color="primary" @click.stop="advancedOptionsShown = !advancedOptionsShown" :disabled="loading" v-if="!mixed">{{advancedLabel}} advanced options</v-btn>
-        </v-card-actions>
-      </v-flex>
-      <v-flex v-if="advancedOptionsShown">
-        <v-container>
-          <v-layout>
-            <v-flex>
-              advanced options go here
-            </v-flex>
-          </v-layout>
-        </v-container>
+      <v-flex v-if="!loading">
+        <v-expansion-panel expand>
+          <v-expansion-panel-content :value="true">
+            <div slot="header">Stats</div>
+            <v-card>
+              <component
+                :is="statsComponent"
+                :transactions="filteredTransactions"
+                :shown-months="shownMonths"
+                v-if="transactions.length > 0"
+              />
+              <v-card-title v-if="transactions.length < 1">There are no results for this period</v-card-title>
+            </v-card>
+          </v-expansion-panel-content>
+          <v-expansion-panel-content>
+            <div slot="header">Actions</div>
+            <v-card>
+              <v-card-actions>
+                <v-btn color="primary" @click.stop="fetchNext" :disabled="loading">Fetch more records</v-btn>
+                <v-btn color="primary" @click.stop="addItemClicked" :disabled="loading" v-if="!mixed">Add new item</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-expansion-panel-content>
+          <v-expansion-panel-content>
+            <div slot="header">Filtering</div>
+            <v-card>
+              <filter-finance
+                :selected-dates="transactions.map(transaction => transaction.due)"
+                @shown-date-selected="shownDateUpdated"
+                @shown-date-reset="shownDateReset"
+                @shown-month-reset="shownMonthReset"
+                @shown-month-selected="shownMonthUpdated"
+              />
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
       </v-flex>
     </v-layout>
   </v-container>
-  </v-card>
 </template>
 <script>
+  import ManageFinanceSummaryStats from './ManageFinanceSummaryStats';
+  import ManageMixedFinanceSummaryStats from './ManageMixedFinanceSummaryStats';
+  import FilterFinance from './FilterFinance';
+
   export default {
+    components: {
+      FilterFinance,
+      ManageFinanceSummaryStats,
+      ManageMixedFinanceSummaryStats
+    },
     name: 'manage-finance-card',
     data () {
       return {
-        advancedOptionsShown: false
+        shownDate: '',
+        shownMonth: '',
+        totalTransactions: [],
+        firstFetched: false,
+        shownPicker: 'date'
       };
     },
     props: {
@@ -54,106 +72,17 @@
       shownMonths: Number
     },
     computed: {
-      advancedLabel () {
-        return this.advancedOptionsShown ? 'Hide' : 'Show';
+      statsComponent () {
+        return this.mixed ? 'manage-mixed-finance-summary-stats' : 'manage-finance-summary-stats';
       },
-      cumulativeVisibleValue () {
-        return this.amountList.reduce((acc, income) => acc + income, 0);
-      },
-      incomeTotal () {
-        if (!this.mixed) return this.cumulativeVisibleValue;
-        return this.allIncomes
-        .map(transaction => +transaction.amount)
-        .reduce((acc, income) => acc + income, 0);
-      },
-      expenditureTotal () {
-        if (!this.mixed) return this.cumulativeVisibleValue;
-        return this.allExpenditures
-        .map(transaction => +transaction.amount)
-        .reduce((acc, income) => acc + income, 0);
-      },
-      allIncomes () {
-        if (!this.mixed) return this.transactions;
-        return this.transactions
-        .filter(transaction => transaction.type === 'Income');
-      },
-      allExpenditures () {
-        if (!this.mixed) return this.transactions;
-        return this.transactions
-        .filter(transaction => transaction.type === 'Expenditure');
-      },
-      dateList () {
-        return this.transactions.map(transaction => transaction.due);
-      },
-      firstDate () {
-        return this.dateList[0];
-      },
-      lastDate () {
-        return this.dateList[this.dateList.length - 1];
-      },
-      meanVisibleValue () {
-        return this.cumulativeVisibleValue / this.transactions.length;
-      },
-      amountList () {
-        return this.transactions.map(transaction => +transaction.amount);
-      },
-      stats () {
-        return [{
-          title: 'Months Shown',
-          value: this.shownMonths,
-          format: 'basic'
-        }, {
-          title: 'Total',
-          value: this.cumulativeVisibleValue,
-          format: 'currency'
-        }, {
-          title: 'Average',
-          value: this.meanVisibleValue,
-          format: 'currency'
-        }, {
-          title: 'Next Due',
-          value: this.firstDate,
-          format: 'date'
-        }, {
-          title: 'Last Due',
-          value: this.lastDate,
-          format: 'date'
-        }];
-      },
-      mixedStats () {
-        return [{
-          title: 'Months Shown',
-          value: this.shownMonths,
-          format: 'basic'
-        }, {
-          title: 'Income Total',
-          value: this.incomeTotal,
-          format: 'currency'
-        }, {
-          title: 'Expenditure Total',
-          value: this.expenditureTotal,
-          format: 'currency'
-        }, {
-          title: 'Balance',
-          value: this.incomeTotal - this.expenditureTotal,
-          format: 'currency'
-        }, {
-          title: 'Average Income',
-          value: this.incomeTotal / this.allIncomes.length,
-          format: 'currency'
-        }, {
-          title: 'Average Expenditure',
-          value: this.expenditureTotal / this.allExpenditures.length,
-          format: 'currency'
-        }, {
-          title: 'Next Due',
-          value: this.firstDate,
-          format: 'date'
-        }, {
-          title: 'Last Due',
-          value: this.lastDate,
-          format: 'date'
-        }];
+      filteredTransactions () {
+        if (this.shownDate !== '') {
+          return this.transactions.filter(transaction => transaction.due === this.shownDate);
+        }
+        if (this.shownMonth !== '') {
+          return this.transactions.filter(transaction => transaction.due.substring(0, transaction.due.lastIndexOf('-')) === this.shownMonth);
+        }
+        return this.transactions;
       }
     },
     methods: {
@@ -162,6 +91,18 @@
       },
       addItemClicked () {
         this.$emit('add-item');
+      },
+      shownMonthUpdated (newMonth) {
+        this.$emit('shown-month-selected', newMonth);
+      },
+      shownMonthReset () {
+        this.$emit('shown-month-reset');
+      },
+      shownDateReset () {
+        this.$emit('shown-date-reset');
+      },
+      shownDateUpdated (newDate) {
+        this.$emit('shown-date-selected', newDate);
       }
     }
   };
