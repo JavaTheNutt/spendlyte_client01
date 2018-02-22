34e8aa4b28005bcdc492e65645df2971a3ae6d58
Joe Wemyss <support@javathenutt.com>
2018-02-22 20:11:22 +0000

    Alert user of save result
    Saving a firestore model now alerts the user of the status of the save through the snackbar

--- src/app/data/firestore/Income.js
+++ src/app/data/firestore/Income.js
@@ -3,11 +3,20 @@ import { addItem } from './util';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import Finance from './Finance';
+import Bus from '@/app/events/bus';
 
 export default class Income extends Finance {
   // fixme add id field for fetching data
   async save () {
-    return await addItem(`transactions/${getUserId()}/incomes`, this.toJson());
+    try {
+      const result = await addItem(`transactions/${getUserId()}/incomes`, this.toJson());
+      Bus.$emit('show-snack', `${this.title} transaction added successfully`);
+      return result;
+    } catch (error) {
+      console.log('an error has occurred', error);
+      Bus.$emit('show-snack', 'Error adding income', 'error');
+      return false;
+    }
   }
 }
 
