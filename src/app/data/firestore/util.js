2f657f18950446e9593780351abfae443631f302
Joe Wemyss <support@javathenutt.com>
2018-02-25 13:08:40 +0000

    Added new firestore utils
    Created utils to fetch an entire collectio from Firestore. Also created some mapping utils to handle converting the query objects into plain JS objects.

--- src/app/data/firestore/util.js
+++ src/app/data/firestore/util.js
@@ -1,4 +1,3 @@
-// @flow
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
@@ -13,6 +12,30 @@ export const addItem = async (path:string, item: any) => {
     return { error: e };
   }
 };
+export const mapDocument = doc => Object.assign({ id: doc.id }, doc.data());
+
+export const mapCollection = collection => collection.docs.map(element => mapDocument(element));
+
+export const fetchMappedCollection = async path => {
+  const result = await fetchCollection(path);
+  if (result.error) return result;
+  console.log();
+  console.log('docs', result.docs);
+  return mapCollection(result);
+};
+
+export const fetchCollection = async (path:string): Promise<Array<any>> => {
+  console.log('attempting to fetch collection at', path);
+  try {
+    const result = await firebase.firestore().collection(path).get();
+    console.log('item assumed fetched successfully, response:', result);
+    return result;
+  } catch (e) {
+    console.warn('an error occurred while fetching data', e);
+    return { error: e };
+  }
+};
+export const fetchCollectionListener = (path: string, collectionUpdated: Function) => firebase.firestore().collection(path).onSnapshot(collectionUpdated);
 
 export const cleanItem = (item: Object) => {
   for (const propName in item) {
@@ -23,3 +46,10 @@ export const cleanItem = (item: Object) => {
   return item;
 };
 
+export const handleSnapshotUpdate = (querySnapshot: any) => {
+  const results = [];
+  querySnapshot.forEach(doc => {
+    results.push(Object.assign({ id: doc.id }, doc.data()));
+  });
+  return results;
+};
--- src/app/data/firestore/util.spec.js
+++ src/app/data/firestore/util.spec.js
@@ -29,4 +29,50 @@ describe('util.js', () => {
       expect(result).to.eql({ error: err });
     });
   });
+  describe('mapDocument', () => {
+    let fakeDoc;
+    beforeEach(() => {
+      fakeDoc = {
+        id: 'someidhere',
+        data () {
+          return {
+            somekey: 'somevalue',
+            someotherkey: 'someothervalue'
+          };
+        }
+      };
+    });
+    it('should correctly map a document', () => {
+      const result = firestoreUtils.mapDocument(fakeDoc);
+      expect(result).to.eql({ id: 'someidhere', somekey: 'somevalue', someotherkey: 'someothervalue' })
+    });
+  });
+  describe('mapCollection', () => {
+    let fakeCollection;
+    beforeEach(() => {
+      fakeCollection = {
+        docs: [{
+          id: 'someidhere',
+          data () {
+            return {
+              somekey: 'somevalue',
+              someotherkey: 'someothervalue'
+            };
+          }
+        }, {
+          id: 'somesecondidhere',
+          data () {
+            return {
+              somekey: 'somesecondvalue',
+              someotherkey: 'someothersecondvalue'
+            };
+          }
+        }]
+      };
+    });
+    it('should correctly map a collection', () => {
+      const result = firestoreUtils.mapCollection(fakeCollection);
+      expect(result).to.eql([{ id: 'someidhere', somekey: 'somevalue', someotherkey: 'someothervalue' }, { id: 'somesecondidhere', somekey: 'somesecondvalue', someotherkey: 'someothersecondvalue' }])
+    });
+  });
 });
