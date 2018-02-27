cd26804f4cb391e86f89bf669ee29093afe8e82a
Joe Wemyss <support@javathenutt.com>
2018-02-27 00:02:38 +0000

    Updated finance service
    Updated the finance service so that it now contains more functionality. The module can now be used to detch all of the base transactions, and has all of the view related tasks, such as emitting messages to the user, covered too.

--- src/app/uiComponents/finance/financeService.js
+++ src/app/uiComponents/finance/financeService.js
@@ -3,12 +3,45 @@ import { fetchMappedCollection } from '@/app/data/firestore/util';
 import Bus from '@/app/events/bus';
 
 export const fetchBaseIncomes = async () => {
-  console.log('fetching base income records');
-  const result = await fetchMappedCollection(`transactions/${fetchId()}/incomes`);
-  if (result.error) {
-    Bus.$emit('show-snack', 'failed to fetch base incomes', 'error');
-    return [];
-  }
-  Bus.$emit('show-snack', `${result.length} income records fetched`, 'success');
+  const result = await getBaseIncomes();
+  showSnack(result, 'income');
   return result;
 };
+
+export const fetchBaseExpenditures = async () => {
+  const result = await getBaseExpenditures();
+  showSnack(result, 'income');
+  return result;
+};
+
+export const fetchBaseTransactions = async () => {
+  const result = await getBaseTransactions();
+  showSnack(result, 'income');
+  return result;
+};
+
+const getBaseExpenditures = async () => {
+  console.log('fetching base expenditure records');
+  const result = await fetchMappedCollection(`transactions/${fetchId()}/expenditures`);
+  return result.error ? [] : mapTransactionTypeCollection(result, 'Expenditure');
+};
+
+const getBaseIncomes = async () => {
+  console.log('fetching base income records');
+  const result = await fetchMappedCollection(`transactions/${fetchId()}/incomes`);
+  return result.error ? [] : mapTransactionTypeCollection(result, 'Income');
+};
+
+const getBaseTransactions = async () => (await getBaseIncomes()).concat(await getBaseExpenditures());
+
+const showSnack = (result, type) => {
+  const msg = `${result.length} ${type || ''} records fetched`;
+  const snackDetails = result.length > 0
+  ? { msg, status: 'success' }
+  : { msg: 'no records fetched', status: 'error' };
+  Bus.$emit('show-snack', snackDetails.msg, snackDetails.msg);
+};
+
+export const mapTransactionTypeCollection = (list, type) => list.map(elem => mapTransactionType(elem, type));
+
+export const mapTransactionType = (elem, type) => Object.assign({ type }, elem);
