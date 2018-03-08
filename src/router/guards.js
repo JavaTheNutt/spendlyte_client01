f5433e4c2b7606cc581fc3f0571ac2df179f42ea
Joe Wemyss <support@javathenutt.com>
2018-03-08 13:06:13 +0000

    Stremlined global router guard
    Streamlined the global router guard that is used called on each route change. Now this guard will just remove the sidenav after every state change, and the component that requires it will be responsible for showing it when its mounted.

--- src/router/guards.js
+++ src/router/guards.js
@@ -1,6 +1,8 @@
 import store from '../store';
+import Bus from '@/app/events/bus';
 
 export const globalAuthGuard = (to, from, next) => {
+  Bus.$emit('reset-sidenav');
   if (!to.meta || !to.meta.noAuth) {
     if (store.state.auth.loggedIn) return next();
     return next('/');
