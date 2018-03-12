217db63a1866f217d054fde833aa830d4ca977a5
Joe Wemyss <support@javathenutt.com>
2018-03-12 10:04:27 +0000

    added references link to home page

--- src/app/uiComponents/screens/home/Home.vue
+++ src/app/uiComponents/screens/home/Home.vue
@@ -3,13 +3,16 @@
     <v-jumbotron color="grey lighten-2">
       <v-container fill-height>
         <v-layout align-center>
-          <v-flex>
+          <v-flex class="text-xs-center text-md-left">
             <h3 class="display-3" id="jumbotronTitle">Welcome to Spend Lyte</h3>
             <span class="subheading" id="jumbotronSubtitle">The only finance app you'll ever need!</span>
             <v-divider class="my-3"/>
             <div class="title mb-3" id="jumbotronInfoMessage">Read a little bit about this application</div>
             <router-link to="/about">
               <v-btn large color="primary" class="mx-0" >About</v-btn>
+            </router-link>
+            <router-link to="/references">
+              <v-btn large color="primary" class="mx-0" >References</v-btn>
             </router-link>
           </v-flex>
         </v-layout>
